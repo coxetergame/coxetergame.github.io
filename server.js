c260aa5f1726411954fec21a25e8732659f3ff06
S. Verona Malone <sasham@fastmail.com>
2020-10-04 18:50:43 -0400

    Get node server running

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/katex', (req, res) => {
-    res.send('Katex goes here');
-});
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
